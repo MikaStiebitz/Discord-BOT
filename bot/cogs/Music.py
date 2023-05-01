c7751a7bc44e0ba711c1eb700876fca384af2b04
Mika <mika.stadelmann@gmail.com>
2023-05-01 14:22:04 +0200

    File rename

--- bot/cogs/Music.py
+++ bot/cogs/Music.py
@@ -4,6 +4,7 @@ import wavelink
 from config import Auth
 from discord.ext import commands
 
+
 class MusicButtonView(discord.ui.View):
     def __init__(self, vc, message):
         super().__init__()
@@ -60,6 +61,7 @@ class MusicButtonView(discord.ui.View):
 class MusicCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.queue = wavelink.Queue()
         self.wavelink_nodes = [
             wavelink.Node(uri=f"http://{Auth.LAVALINK_HOST}:{Auth.LAVALINK_PORT}", password=f"{Auth.LAVALINK_PASSWORD}")
         ]
@@ -69,17 +71,29 @@ class MusicCog(commands.Cog):
         await self.bot.wait_until_ready()
         await wavelink.NodePool.connect(client=self.bot, nodes=self.wavelink_nodes)
 
-    @commands.command()
-    async def play(self, ctx: commands.Context, *, search: str) -> None:
+    @wavelink.WavelinkMixin
+    @commands.Cog.listener()
+    async def on_wavelink_track_end(self, player: wavelink.Player, track: wavelink.Track):
+        ctx = player.ctx
+        vc: player = ctx.voice_client
+        if vc.loop:
+            return await vc.play(track)
+        
+        next_song = vc.queue.get()
+        await vc.play(next_song)
+        await ctx.send(f"Now playing {next_song.title}")
+
+    @commands.hybrid_command()
+    async def play(self, ctx: commands.Context, *, music: str):
         if not ctx.voice_client:
             vc: wavelink.Player = await ctx.author.voice.channel.connect(cls=wavelink.Player)
         else:
             vc: wavelink.Player = ctx.voice_client
 
-        if re.match(r"^(http(s)?://)?((w){3}.)?youtu(be|.be)?(\.com)?/.+", search):
-            track = await wavelink.YouTubeTrack.search(search, return_first=True)
+        if re.match(r"^(http(s)?://)?((w){3}.)?youtu(be|.be)?(\.com)?/.+", music):
+            track = await wavelink.YouTubeTrack.search(music, return_first=True)
         else:
-            tracks = await wavelink.YouTubeTrack.search(search)
+            tracks = await wavelink.YouTubeTrack.search(music)
             if not tracks:
                 await ctx.send("No search results found.")
                 return
@@ -102,19 +116,67 @@ class MusicCog(commands.Cog):
             track = tracks[reactions.index(str(reaction.emoji))]
             await message.delete()
 
-        await vc.play(track)
-        message = await ctx.send(f"Now playing: {track.title} ({track.author})")
-        view = MusicButtonView(vc, message)
-        message = await ctx.send(view=view)
+        if vc.is_playing():
+            print("queue")
 
-        if not vc.is_playing():
-            await ctx.send("Playback ended.")
-            await vc.disconnect()
+            vc.queue.put(item=track)
+
+            await ctx.send(embed=discord.Embed(
+                title=track.title,
+                url=track.uri,
+                author=ctx.author,
+                description=f"Queued {track.title} in {vc.channel}"
+            ))
+        else:
+            await vc.play(track)
+            message = await ctx.send(f"Now playing: {track.title} ({track.author})")
+            view = MusicButtonView(vc, message)
+            message = await ctx.send(view=view)
 
     @commands.command()
-    async def disconnect(self, ctx: commands.Context) -> None:
+    async def stopMusic(self, ctx: commands.Context):
         vc: wavelink.Player = ctx.voice_client
         await vc.disconnect()
+
+    @commands.command()
+    async def skip(self, ctx):
+        print("skip")
+        vc = ctx.voice_client
+        if vc:
+            if not vc.is_playing():
+                return await ctx.send("Nothing is playing.")
+            if vc.queue.is_empty:
+                return await vc.stop()
+
+            await vc.seek(vc.track.length * 1000)
+            if vc.is_paused():
+                await vc.resume()
+        else:
+            await ctx.send("The bot is not connected to a voice channel.")
+
+
+    @commands.command()
+    async def pause(self, ctx):
+        vc = ctx.voice_client
+        if vc:
+            if vc.is_playing() and not vc.is_paused():
+                await vc.pause()
+            else:
+                await ctx.send("Nothing is playing.")
+        else:
+            await ctx.send("The bot is not connected to a voice channel")
+
+
+    @commands.command()
+    async def resume(self, ctx):
+        vc = ctx.voice_client
+        if vc:
+            if vc.is_paused():
+                await vc.resume()
+            else:
+                await ctx.send("Nothing is paused.")
+        else:
+            await ctx.send("The bot is not connected to a voice channel")
 
 async def setup(client):
     await client.add_cog(MusicCog(client))
